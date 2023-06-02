0ac8bc728ff1f042407b49f7b805b608b41703da
NicoletteBoggs <nrb1628@gmail.com>
2023-06-01 20:03:33 -0600

    bio

--- src/components/About.js
+++ src/components/About.js
@@ -15,8 +15,28 @@ function About() {
                   className="rounded img-fluid"
                 />
                 <p className="card-text">
-                  Nicolette. Food and mountain enthusiast. Baby coder.
-                  University of Denver alumni.
+                  Hi there! My name is Nicolette, and I'm a recent graduate from
+                  University of Denver with a certificate in coding. As a
+                  Frontend Web Developer, I'm passionate about creating
+                  beautiful, responsive, and user-friendly websites that provide
+                  a great user experience. While I don't have any professional
+                  work experience yet, I've spent the past few years honing my
+                  skills and building my portfolio. I have experience with a
+                  variety of frontend technologies, including HTML, CSS,
+                  JavaScript, and popular frameworks like React. I'm also
+                  proficient in design tools such as Figma and Sketch.
+                  Throughout my academic career, I've had the opportunity to
+                  work on several projects that have challenged me and helped me
+                  grow as a developer. One of my favorite projects was a mock-up
+                  of a hotel called the Mallie, where I worked very in depth on
+                  the spa page- making it aesthetically pleasing as well as
+                  functional, with added pricing and booking features. I'm
+                  excited to begin my career as a Frontend Web Developer and am
+                  eager to contribute my skills and knowledge to a dynamic and
+                  innovative team. Please feel free to connect with me on
+                  LinkedIn or email me at nicoletteboggs@gmail.com to learn more
+                  about my experience and skills. You can check out my Github
+                  here: https://github.com/NicoletteBoggs
                   <br />
                   <br />
                 </p>
